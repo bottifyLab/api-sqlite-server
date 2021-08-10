e505832480e58a7f8a105ba6f3a2333f709f88d2
sellercoder <veniamin4e@gmail.com>
2021-08-10 14:36:13 +1000

    add pug view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Render, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  index() {
+    return;
   }
+
+  @Get('/users')
+  getUsers() {
+    return [{id: 1, name: 'Benjamin'}]
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors()
+
+  app.setBaseViewsDir(join(__dirname,'../views'));
+  app.setViewEngine('pug');
+
   await app.listen(process.env.PORT || 3000);
+
 }
 bootstrap();
