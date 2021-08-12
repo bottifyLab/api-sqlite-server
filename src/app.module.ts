720af957edb7386d0171c2aa0735a8f17a35e716
sellercoder <veniamin4e@gmail.com>
2021-08-13 09:04:06 +1000

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     SequelizeModule.forRoot({
         dialect: 'sqlite',
-        storage: './data.sqlite',
+        storage: './d.sqlite',
         models: [User, Category, Role, UserRoles],
         autoLoadModels: true
     }),
